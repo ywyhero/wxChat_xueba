abf163b6b31485bcee4c855a496dae0ac3a65c9f
yaoweiyu <yaoweiyu@peilian.com>
2018-06-11 18:23:47 +0800

    优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,109 +10,109 @@ Page({
     phoneNumber: '',
     username: '',
     classes: [{
-        id: 0,
-        value: '-请选择年级-'
-      }, {
-        id: 1,
-        value: '一年级'
-      }, {
-        id: 2,
-        value: '二年级'
-      }, {
-        id: 3,
-        value: '三年级'
-      }, {
-        id: 4,
-        value: '四年级'
-      }, {
-        id: 5,
-        value: '五年级'
-      }, {
-        id: 6,
-        value: '六年级'
-      }, {
-        id: 7,
-        value: '初一'
-      }, {
-        id: 8,
-        value: '初二'
-      }, {
-        id: 9,
-        value: '初三'
-      }, {
-        id: 10,
-        value: '高一'
-      }, {
-        id: 11,
-        value: '高二'
-      }, {
-        id: 83,
-        value: '高三'
-      }, {
-        id: 159,
-        value: '其他'
+      id: 0,
+      value: '-请选择年级-'
+    }, {
+      id: 1,
+      value: '一年级'
+    }, {
+      id: 2,
+      value: '二年级'
+    }, {
+      id: 3,
+      value: '三年级'
+    }, {
+      id: 4,
+      value: '四年级'
+    }, {
+      id: 5,
+      value: '五年级'
+    }, {
+      id: 6,
+      value: '六年级'
+    }, {
+      id: 7,
+      value: '初一'
+    }, {
+      id: 8,
+      value: '初二'
+    }, {
+      id: 9,
+      value: '初三'
+    }, {
+      id: 10,
+      value: '高一'
+    }, {
+      id: 11,
+      value: '高二'
+    }, {
+      id: 83,
+      value: '高三'
+    }, {
+      id: 159,
+      value: '其他'
     }],
     classId: 0,
     subjects: [{
-        id: 0,
-        value: '-请选择科目-'
-      }, {
-        id: 66,
-        value: '语文'
-      }, {
-        id: 67,
-        value: '数学'
-      }, {
-        id: 68,
-        value: '英语'
-      }, {
-        id: 69,
-        value: '物理'
-      }, {
-        id: 70,
-        value: '化学'
-      }, {
-        id: 71,
-        value: '生物'
-      }, {
-        id: 72,
-        value: '政治'
-      }, {
-        id: 73,
-        value: '历史'
-      }, {
-        id: 74,
-        value: '地理'
-      }, {
-        id: 76,
-        value: '奥数'
-      }, {
-        id: 158,
-        value: '技术'
-      }, {
-        id: 160,
-        value: '信息技术'
-      }, {
-        id: 161,
-        value: '科学'
-      }, {
-        id: 163,
-        value: 'IB'
-      }, {
-        id: 164,
-        value: 'alevel'
-      }, {
-        id: 165,
-        value: '雅思'
-      }, {
-        id: 166,
-        value: '托福'
-      }, {
-        id: 167,
-        value: '竞赛'
-      }, {
-        id: 168,
-        value: '高数'
+      id: 0,
+      value: '-请选择科目-'
+    }, {
+      id: 66,
+      value: '语文'
+    }, {
+      id: 67,
+      value: '数学'
+    }, {
+      id: 68,
+      value: '英语'
+    }, {
+      id: 69,
+      value: '物理'
+    }, {
+      id: 70,
+      value: '化学'
+    }, {
+      id: 71,
+      value: '生物'
+    }, {
+      id: 72,
+      value: '政治'
+    }, {
+      id: 73,
+      value: '历史'
+    }, {
+      id: 74,
+      value: '地理'
+    }, {
+      id: 76,
+      value: '奥数'
+    }, {
+      id: 158,
+      value: '技术'
+    }, {
+      id: 160,
+      value: '信息技术'
+    }, {
+      id: 161,
+      value: '科学'
+    }, {
+      id: 163,
+      value: 'IB'
+    }, {
+      id: 164,
+      value: 'alevel'
+    }, {
+      id: 165,
+      value: '雅思'
+    }, {
+      id: 166,
+      value: '托福'
+    }, {
+      id: 167,
+      value: '竞赛'
+    }, {
+      id: 168,
+      value: '高数'
     }],
     subjectId: 0,
     indicatorDots: true,
@@ -133,107 +133,107 @@ Page({
         subject: '初三 数学单科提高',
         score: '93分',
         count: 160
-      },{
+      }, {
         username: '*培言',
         subject: '高一 数学单科提高',
         score: '28分',
         count: 120
-      },{
+      }, {
         username: '*益瑄',
         subject: '高二 物理单科提高',
         score: '15分',
         count: 160
-      },{
+      }, {
         username: '*镜名',
         subject: '初三 数学物理总提分',
         score: '76分',
         count: 240
-      },{
+      }, {
         username: '*俊琪',
         subject: '初三 化学单科提高',
         score: '27分',
         count: 120
-      },{
+      }, {
         username: '*佳悦',
         subject: '高二 数理化平均提分',
         score: '26分',
         count: 120
-      },{
+      }, {
         username: '*邱涵',
         subject: '六年级 英语单科提高',
         score: '21分',
         count: 120
-      },{
+      }, {
         username: '*笑笑',
         subject: '高二 数学单科提高',
         score: '37分',
         count: 320
-      },{
+      }, {
         username: '*颜',
         subject: '初三 英语提高',
         score: '23分',
         count: 120
-      },{
+      }, {
         username: '*鸿',
         subject: '六年级 总分提高',
         score: '53分',
         count: 160
-      },{
+      }, {
         username: '*涵',
         subject: '高二 数学单科提高',
         score: '42分',
         count: 240
-      },{
+      }, {
         username: '*可欣',
         subject: '高三 数学竞赛排名第1',
         score: '',
         count: 120
-      },{
+      }, {
         username: '*欣宇',
         subject: '中考全科提分',
         score: '53分',
         count: 120
-      },{
+      }, {
         username: '*念',
         subject: '五年级 语文提高',
         score: '18分',
         count: 100
-      },{
+      }, {
         username: '*若姿',
         subject: '初三 数学提高',
         score: '27分',
         count: 80
-      },{
+      }, {
         username: '*佳',
         subject: '高一 数学单科提高',
         score: '35分',
         count: 160
-      },{
+      }, {
         username: '*涛',
         subject: '高考全科提分',
         score: '109分',
         count: 160
-      },{
+      }, {
         username: '*杰盈',
         subject: '初二 物理成绩满分',
         score: '',
         count: 240
-      },{
+      }, {
         username: '*梓怡',
         subject: '高考全科提分',
         score: '109分',
         count: 160
-      },{
+      }, {
         username: '*雨诺',
         subject: '高考全科提分',
         score: '94分',
         count: 120
-      },{
+      }, {
         username: '*圆',
         subject: '初三 物理竞赛二等奖',
         score: '',
         count: 80
-      },{
+      }, {
         username: '*梦晨',
         subject: '高三 数学提高',
         score: '37分',
@@ -248,8 +248,8 @@ Page({
     let top = 0;
     let self = this
     this.data.timer = setInterval(scroll, 20);
-    function scroll () {
-      if(top >= -(ulHeight)){
+    function scroll() {
+      if (top >= -(ulHeight)) {
         top -= 1;
         self.setData({
           top: top
@@ -273,15 +273,24 @@ Page({
         })
       }
     });
-    
-
   },
-  getPhoneNumber(e){
+  onShareAppMessage:  (res) => {
+    if (res.from === 'menu') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '学霸1对1在线辅导',
+      path: '/page/index/index',
+      imageUrl: './../../assets/share.png'
+    }
+  },
+  getPhoneNumber(e) {
     this.setData({
       phoneNumber: e.detail.value
     })
   },
-  getUserName(e){
+  getUserName(e) {
     this.setData({
       username: e.detail.value
     })
@@ -299,40 +308,40 @@ Page({
     })
   },
   toTop(e) {
-    wx.pageScrollTo({  
+    wx.pageScrollTo({
       scrollTop: 0
     })
   },
-  register(){
-    if(!this.data.username){
+  register() {
+    if (!this.data.username) {
       wx.showToast({
         title: '用户名不能为空',
         icon: 'none'
       })
       return
     }
-    if(this.data.username.length < 2){
+    if (this.data.username.length < 2) {
       wx.showToast({
         title: '用户名长度不能低于两位',
         icon: 'none'
       })
       return
     }
-    if(!this.data.phoneNumber) {
+    if (!this.data.phoneNumber) {
       wx.showToast({
         title: '手机号码不能为空',
         icon: 'none'
       })
       return
     }
-    if(!this.data.classId) {
+    if (!this.data.classId) {
       wx.showToast({
         title: '请选择年级',
         icon: 'none'
       })
       return
     }
-    if(!this.data.subjectId) {
+    if (!this.data.subjectId) {
       wx.showToast({
         title: '请选择科目',
         icon: 'none'
@@ -342,7 +351,7 @@ Page({
     wx.request({
       url: 'https://www.xuebacoming.com/reg/reg_register',
       method: 'POST',
-      header:{
+      header: {
         'Content-Type': 'application/x-www-form-urlencoded'
       },
       data: {
@@ -354,27 +363,27 @@ Page({
       },
       success: (data) => {
         let res = data.data;
-        if(res.code === 0) {
+        if (res.code === 0) {
           wx.showToast({
             title: '注册成功',
             icon: 'success'
           })
-        } else if(res.code === -1){
+        } else if (res.code === -1) {
           wx.showToast({
             title: '注册失败',
             icon: 'none'
           })
-        } else if(res.code === 1001){
+        } else if (res.code === 1001) {
           wx.showToast({
             title: '手机号码重复',
             icon: 'none'
           })
-        } else if(res.code === 1002){
+        } else if (res.code === 1002) {
           wx.showToast({
             title: '手机号码不正确',
             icon: 'none'
           })
-        } else if(res.code === 1003){
+        } else if (res.code === 1003) {
           wx.showToast({
             title: '用户名长度不能低于两位',
             icon: 'none'
